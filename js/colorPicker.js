7d7a133e6b3d015777c1ce81a943f3a90b8bd623
Mike Baker <mikebaker@alumni.cedarville.edu>
2022-03-21 14:56:25 -0400

    Add color picker page

---
+++ js/colorPicker.js
@@ -0,0 +1,8 @@
+//
+//Color Picker
+//
+
+function changeURL(){
+    document.getElementById("image123").src="https://s7d1.scene7.com/is/image/mcdonalds/t-mcdonalds-Cheeseburger-1:1-3-product-tile-desktop?wid=829&hei=515&dpr=off";
+    alert("WOW!")
+}
